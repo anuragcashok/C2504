d0fab10c26db19a3de64b55d9961a070762bae18
anuragcashok <84575763+anuragcashok@users.noreply.github.com>
2024-08-02 18:10:49 +0530

    Qstn1(SeriesLoop).cs

---
+++ 02-08-2024/Qstn1.cs
@@ -0,0 +1,34 @@
+using System;
+
+
+namespace Psrograms
+{
+    internal class Program
+    {
+        static void PrintSeries10_12_14(int numOfTerms)
+        {
+            int term = 10;
+            for (int I = 1; I <= numOfTerms; I++)
+            {
+                Console.Write($"{term} ");
+                term = term + 2;
+            }
+        }
+        static void TestPrintSeries10_12_14()
+        {
+            Console.Write("Enter number of terms:");
+            int numOfTerms = int.Parse(Console.ReadLine());
+            Console.Write("Series: ");
+            PrintSeries10_12_14(numOfTerms);
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args) //user: p
+        {
+            Console.WriteLine("--------------TestPrintSeries10_12_14...--------------");
+            TestPrintSeries10_12_14();
+            Console.WriteLine("--------------End TestPrintSeries10_12_14...--------------");
+            Console.ReadKey();
+        }
+    }
+}
